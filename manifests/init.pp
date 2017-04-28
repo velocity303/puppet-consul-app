c09f5d045dd7dacc841bba3b37887f43ac5972d8
James E. Jones <james.jones@puppet.com>
2017-04-28 17:18:39 -0500

    update app definition

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,14 +4,14 @@ application consul_cluster (
 
   $consul_count = collect_component_titles($nodes, Consul_cluster::Node)
 
-  consul_cluster::head { "${name}-head":
+  consul_cluster::head { "${name}":
     datacenter => $datacenter,
-    export     => Consulhead["head-${name}"],
+    export     => Consulhead["${name}"],
   }
 
   $consul_count.each |$j| {
   consul_cluster::node { $j:
-    consume => Consulhead["head-${name}"],
+    consume => Consulhead["${name}"],
   }
 
 }
